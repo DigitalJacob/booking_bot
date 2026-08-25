50277e777c4ed66a2f84ff4863ee05853b7509e9
Jackob <yashava@yandex.ru>
2026-08-25 23:04:54 +0300

    Add: role/locale filters

--- app/infrastructure/database/repositories/users.py
+++ app/infrastructure/database/repositories/users.py
@@ -119,3 +119,17 @@ async def change_user_banned_status(
             params=(banned, user_id),
         )
     logger.info("Updated banned=%s for user %d", banned, user_id)
+
+
+async def get_user_role(
+        conn: AsyncConnection,
+        *,
+        user_id: int,
+) -> UserRole | None:
+    async with conn.cursor() as cursor:
+        await cursor.execute(
+            query="SELECT role FROM users WHERE user_id = %s;",
+            params=(user_id, ),
+        )
+        row = await cursor.fetchone()
+    return UserRole(row[0]) if row else None
